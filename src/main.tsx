901bd3e66d840437e1ba4531b61ee96290deeda0
Robert Santos <robertdahora@gmail.com>
2026-01-25 15:53:52 +0100

    feat: enhance theme integration with MUI by adding ThemeProvider and custom theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { StyledEngineProvider } from '@mui/material/styles';
+import {
+  createTheme,
+  StyledEngineProvider,
+  ThemeProvider
+} from '@mui/material/styles';
 import GlobalStyles from '@mui/material/GlobalStyles';
+
+const theme = createTheme({
+  cssVariables: true,
+  colorSchemes: { light: true, dark: true }
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <StyledEngineProvider enableCssLayer injectFirst>
-      <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
-      <App />
+    <StyledEngineProvider enableCssLayer>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+        <App />
+      </ThemeProvider>
     </StyledEngineProvider>
   </React.StrictMode>
 );
