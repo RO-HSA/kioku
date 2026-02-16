37a88dd6492f83e76c00d175eb0aa22a88ae61e6
Robert Santos <robertdahora@gmail.com>
2026-02-16 23:47:50 +0100

    feat: add OAuth form authorization for anilist and update URL handling

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,4 @@
-import { ListType } from '@/types/List';
+import { ListType, Provider } from '@/types/List';
 
 export const buildEntityUrl = (
   provider: string,
@@ -10,5 +10,16 @@ export const buildEntityUrl = (
       return `https://myanimelist.net/${type}/${id}`;
     default:
       return `https://myanimelist.net/${type}/${id}`;
+  }
+};
+
+export const buildRegisterUrl = (provider: Provider) => {
+  switch (provider) {
+    case Provider.MY_ANIME_LIST:
+      return 'https://myanimelist.net/register.php';
+    case Provider.ANILIST:
+      return 'https://anilist.co/signup';
+    default:
+      return 'https://myanimelist.net/register.php';
   }
 };
