4594936c290e99bf63912f1ebf85ad339786ecd5
Robert Santos <robertdahora@gmail.com>
2026-04-05 18:57:28 +0200

    feat: refactor route paths to use PathName enum for better maintainability

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,31 @@
 import { createBrowserRouter } from 'react-router';
+
 import NowPlaying from './components/anime/NowPlaying';
 import ListDataGrid from './layouts/ListDataGrid';
 import PageLayout from './layouts/PageLayout';
 
+export enum PathName {
+  ROOT = '/',
+  NOW_PLAYING = '/now-playing',
+  SEARCH = '/search'
+}
+
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: PathName.ROOT,
     element: <PageLayout />,
     children: [
       {
-        path: '/',
+        path: PathName.ROOT,
         element: <ListDataGrid />
       },
       {
-        path: '/now-playing',
+        path: PathName.NOW_PLAYING,
         element: <NowPlaying />
+      },
+      {
+        path: PathName.SEARCH,
+        element: <ListDataGrid />
       }
     ]
   }
